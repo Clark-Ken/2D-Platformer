f37efb465b8bb3c315d6c8bcedf20a60c310e8a5
Vadimas <vadimas173@gmail.com>
2019-01-29 16:42:01 +0200

    The level generates

---
+++ 2D Platformer/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    public Transform[] startingPositions;
+
+    public GameObject[] rooms;  //index 0 - LR, index 1 - LRB, index 2 - LRT, index 3 - LRTB
+
+    public LayerMask room;
+
+    private int direction;
+
+    public float moveAmount;
+    private float timeBtwRoom;
+    public float startTimeBtwRoom;
+    //Boarder
+    public float minX;
+    public float maxX;
+    public float minY;
+
+    private bool stopGeneration = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rndStartingPos = Random.Range(0, startingPositions.Length);
+        transform.position = startingPositions[rndStartingPos].position;
+
+        Instantiate(rooms[0], transform.position, Quaternion.identity);
+
+        direction = Random.Range(1, 8);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwRoom <= 0 && stopGeneration == false)
+        {
+            MoveRoom();
+            timeBtwRoom = startTimeBtwRoom;
+        }
+        else
+        {
+            timeBtwRoom -= Time.deltaTime; 
+        }
+    }
+
+    void MoveRoom()
+    {
+        if (direction == 1 || direction == 2 || direction == 3)   //Move right
+        {
+            if (transform.position.x < maxX)
+            {
+                Vector2 newPosition = new Vector2(transform.position.x + moveAmount, transform.position.y);
+                transform.position = newPosition;
+
+                int rndRoom = Random.Range(0, rooms.Length);
+                Instantiate(rooms[rndRoom], transform.position, Quaternion.identity);
+
+                direction = Random.Range(1, 8);
+
+                if (direction == 4)
+                {
+                    direction = 2;
+                }
+                else if (direction == 5)
+                {
+                    direction = 3;
+                }
+                else if (direction == 6)
+                {
+                    direction = 7;
+                }
+            }
+            else
+            {
+                direction = 7;
+            }
+        }
+        else if (direction == 4 || direction == 5 || direction == 6)   //Move left
+        {
+            if (transform.position.x > minX)
+            {
+                Vector2 newPosition = new Vector2(transform.position.x - moveAmount, transform.position.y);
+                transform.position = newPosition;
+
+                int rndRoom = Random.Range(0, rooms.Length);
+                Instantiate(rooms[rndRoom], transform.position, Quaternion.identity);
+
+                direction = Random.Range(4, 8);
+            }
+            else
+            {
+                direction = 7;
+            }   
+        }
+        else if (direction == 7)   //Move down
+        {
+            if (transform.position.y > minY)
+            {
+                //Invisible circle only detects rooms
+                Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, room);
+
+                //If the room doesn't have a bottom opening
+                if (roomDetection.GetComponent<RoomType>().type != 1 && roomDetection.GetComponent<RoomType>().type != 3)
+                {
+                    roomDetection.GetComponent<RoomType>().RoomDestruction();
+
+                    int rndBottomRoom = Random.Range(1, 4);
+
+                    if (rndBottomRoom == 2)
+                    {
+                        rndBottomRoom = 1;
+                    }
+
+                    Instantiate(rooms[rndBottomRoom], transform.position, Quaternion.identity);
+                }
+
+                Vector2 newPosition = new Vector2(transform.position.x, transform.position.y - moveAmount);
+                transform.position = newPosition;
+
+                int rndRoom = Random.Range(2, 3);
+                Instantiate(rooms[rndRoom], transform.position, Quaternion.identity);
+
+                direction = Random.Range(1, 8);
+            }
+            else    //Stop generation
+            {
+                stopGeneration = true;
+            }
+        }        
+    }
+}
---
+++ 2D Platformer/Assets/Scripts/SpawnTiles.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTiles : MonoBehaviour
+{
+    public GameObject[] tiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rnd = Random.Range(0, tiles.Length);
+        GameObject instance = Instantiate(tiles[rnd], transform.position, Quaternion.identity);
+        instance.transform.parent = transform;  //child = parent, the instance variable is a child, of whatever spawned it (Room gameobject)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
