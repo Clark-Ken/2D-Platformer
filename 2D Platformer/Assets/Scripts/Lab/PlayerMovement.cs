77e419fa0438f90f85b3f851a1186e6768cc10ae
Clark-Ken <vadimas173@gmail.com>
2019-03-02 19:30:35 +0200

    Grass area.

--- 2D Platformer/Assets/Scripts/Lab/PlayerMovement.cs
+++ 2D Platformer/Assets/Scripts/Lab/PlayerMovement.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+    public float jumpTakeOffSpeed;
+    private float inputHorizontal;
+    private float inputVertical;
+    public float distance;
+
+    private Rigidbody2D rb;
+    public LayerMask ladderLayer;
+
+    private bool isClimbing;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        inputHorizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpTakeOffSpeed);
+        }
+
+        rb.velocity = new Vector2(inputHorizontal * speed, rb.velocity.y);
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distance, ladderLayer);
+
+        if (hitInfo.collider != null)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                isClimbing = true;
+            }
+        }
+        else
+        {
+            isClimbing = false;
+        }
+
+        if (isClimbing == true)
+        {
+            inputVertical = Input.GetAxis("Vertical");
+            rb.velocity = new Vector2(rb.velocity.x, inputVertical * speed);
+            rb.gravityScale = 0;
+        }
+        else
+        {
+            rb.gravityScale = 5;
+        }
     }
 }
