bd2d0dbe9ab2a6626ff18a39b75b9c8359f3417d
Clark-Ken <vadimas173@gmail.com>
2019-03-02 20:09:28 +0200

    5 sounds.

---
+++ 2D Platformer/Assets/Scripts/Lab/PlayRandomly.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRandomly : MonoBehaviour
+{
+    private AudioSource audioS;
+    public AudioClip clip;
+
+    public int maxRnd;
+
+    bool isOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioS = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int rnd = Random.Range(0, maxRnd);
+
+        if (rnd == 10 && !isOn)
+        {
+            audioS.Play();
+        }
+
+        if (audioS.isPlaying)
+        {
+            isOn = true;
+        }
+        else
+        {
+            isOn = false;
+        }
+
+    }
+}
