7fad6f90be6efb534ed3d5667876d1d8c3fe051f
Vadimas <vadimas173@gmail.com>
2019-03-19 17:10:06 +0200

    .

---
+++ 2D Platformer/Assets/Lab2/ForceCube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceCube : MonoBehaviour
+{
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log("Force Animation");
+            Vector3 vec = new Vector3(0, 1000, 0);
+
+            rb.AddForce(vec, ForceMode.Acceleration);
+            rb.useGravity = true;
+        }
+    }
+}
