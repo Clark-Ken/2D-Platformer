ee0bd79f9a45651920673d56fae5be8e77e1ded9
Vadimas <vadimas173@gmail.com>
2019-04-15 12:30:42 +0300

    MainMenu

---
+++ 2D Platformer/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
+    public string gameScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void OpenOptions()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+    public void OpenScores()
+    {
+
+    }
+
+    public void OpenAbout()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit game");
+
+        Application.Quit();
+    }
+}
